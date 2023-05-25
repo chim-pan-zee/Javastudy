ed97df670545bcdd66e1fb3ca63e373c99e7f2fc
chim-pan-zee <127918752+chim-pan-zee@users.noreply.github.com>
2023-05-25 11:43:17 +0900

    Create ServeClass.java

---
+++ GoodClass/ServeClass.java
@@ -0,0 +1,13 @@
+package sec03.LoginSystem;
+
+public class ServeClass {
+	
+
+	
+	public static void main(String[] args) {
+		 Movie mv = new Movie("기생충", "봉준호", 298);
+		 System.out.println(mv.toString());
+		 
+		
+	 }
+}
